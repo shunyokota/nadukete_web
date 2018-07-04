a7c7f1414bf1e8588883fe61b1777cb41aac011b
shunyokota <shunyokota@gmail.com>
2018-07-04 20:33:41 +0900

    font awsomeインストール

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,20 +5,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="content-language" content="ja">
     <title>なづけて</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 
 <body>
+
+
+<header id="main-header">
+    <div id="header-inner">
+        <a href="/">
+            <img id="logo" src="{{ asset('/images/logo.svg') }}" alt="なづけて">
+        </a>
+<h1 id="service-title">なづけて</h1>
+<a id="hamburger" @click="registry_visible = true">
+    <i class="fa fa-bars fa-2x"></i>
+</a>
+</div>
+
+<el-dialog :visible.sync="registry_visible" custom-class="main-menu-dialog" v-cloak="v-cloak">
+<ul>
+
+</ul>
+</el-dialog>
+</header>
+
+
 <div class="container">
 <nav class="navbar navbar-default">
-    <!-- Navbar Contents -->
+<!-- Navbar Contents -->
 </nav>
 @if (!Auth::user())
-    <a href="/auth/twitter">Twitterで新規登録/ログイン</a>
+<a href="/auth/twitter">Twitterで新規登録/ログイン</a>
 @endif
 </div>
 @if (Auth::user())
-    {{ Auth::user()->name }}
-    <a href="/auth/logout">ログアウト</a>
+{{ Auth::user()->name }}
+<a href="/auth/logout">ログアウト</a>
 @endif
 
 @yield('content')
