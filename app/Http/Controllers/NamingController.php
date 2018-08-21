5faeec8332d0e13f6cd63c70f13420e58ab34ebc
shunyokota <shunyokota@gmail.com>
2018-08-21 23:19:57 +0900

    修正

--- app/Http/Controllers/NamingController.php
+++ app/Http/Controllers/NamingController.php
@@ -39,7 +39,7 @@ class NamingController extends Controller
             $star->naming_id = $naming_id;
         }
         $star->point = $request->point;
-        $naming->totalPoint = $naming->calculateTotalPoint();
+        $naming->total_point = $naming->calculateTotalPoint();
 
         DB::beginTransaction();
         $star->save();
