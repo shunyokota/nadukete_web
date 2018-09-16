93fabcf968acefb2ed844a3549945db6e0ba663d
shunyokota <shunyokota@gmail.com>
2018-09-16 12:12:11 +0900

    メタタグ修正

--- resources/views/themes/detail.blade.php
+++ resources/views/themes/detail.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('meta')
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@nadukete_dev" />
+    <meta property="og:url" content="{{Request::fullUrl()}}" />
+    <meta property="og:title" content="この事象になづけてください" />
+    {{--<meta property="og:description" content="記事の要約（ディスクリプション）" />--}}
+
     <meta property="og:image" content="{{env('AWS_S3_URL')}}theme_{{$theme->id}}.jpg" />
 @endsection
 
