2b0e0cbf03d01eff9cea2734c612d220d24ab8e8
shunyokota <shunyokota@gmail.com>
2020-09-07 13:21:18 +0900

    説明文追記

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
     <script src="/js/themes.js"></script>
+
+    <div class="content">
+        <h3>なづけてとは</h3>
+        日々を過ごしていて、
+        <ul>
+            <li>炊事場でスプーンに水道水がダイレクトに当たってびしゃーってなった時</li>
+            <li>職場の人とと帰り道に一緒になりたくないから遠回りして帰る時</li>
+        </ul>
+        そんなよくありがちな事象に遭遇した時、それを何と呼べばいいか名前が無いことでもどかしい思いをしたことはありませんか？
+        「なづけて」は、そんな事象にみんなで名前をつけていくサイトです。
+    </div>
+
     <div id="theme-content" class="content">
         @foreach($themes as $theme)
             @include('themes.partials.theme', ['theme' => $theme, 'last' => $loop->last])
